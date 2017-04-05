fab12e9d6515db8e75b5cebd7e6b843a0a056c06
Prygl <lrni13@student.aau.dk>
2017-04-05 17:08:56 +0200

    ting og sager

---
+++ Assets/lightningScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightningScript : MonoBehaviour {
+    public ParticleSystem lightningEmitters;
+    private ParticleSystem player;
+    private float time = 5f;
+	
+	
+	// Update is called once per frame
+	void Start () {
+        player = lightningEmitters;
+        StartCoroutine(emitLightning());
+	}
+
+   IEnumerator emitLightning()
+    {
+        while (true)
+        {
+            
+            
+            player.Play();
+            
+          
+            yield return new WaitForSeconds(time );
+            time = Random.Range(10, 20);
+            player.Stop();
+        }
+        
+       
+        
+    }
+}
