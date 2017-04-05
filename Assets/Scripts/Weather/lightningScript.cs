c19d3d935ea520853ab76c6bb55cf6601e010bf1
Prygl <lrni13@student.aau.dk>
2017-04-05 23:24:10 +0200

    Mere Lukas

---
+++ Assets/Scripts/Weather/lightningScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightningScript : MonoBehaviour {
+    public ParticleSystem[] lightningEmitters;
+    public bool emitLight = true;
+    private ParticleSystem player;
+    private float time = 15f;
+	
+	
+	// Update is called once per frame
+	void Start () {
+       
+        StartCoroutine(emitLightning());
+	}
+
+   IEnumerator emitLightning()
+    {
+        while (emitLight)
+        {
+
+            player = lightningEmitters[Random.Range(0, lightningEmitters.Length)];
+            player.Play();          
+          
+            yield return new WaitForSeconds(time);
+            time = Random.Range(15, 25);
+            player.Stop();
+        }
+        
+       
+        
+    }
+}
