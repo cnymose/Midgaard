5f4e921f3d7223ba0f4068ec0aa160c8c4b3d277
Prygl <lrni13@student.aau.dk>
2017-04-24 13:55:41 +0200

    Terrain placement fix

--- Assets/Scripts/TerrainManager/TerrainManager.cs
+++ Assets/Scripts/TerrainManager/TerrainManager.cs
@@ -65,8 +65,34 @@ namespace Midgaard
             }
             // Setting current Terrain to the new terrain which the player has recently collided with
             currentTerrain = newTerrain;
-            if(!startTerrains.Contains(newTerrain))
-                    startTerrains.Add(newTerrain); // Adding new terrain to start terrain array making it always visible
+            if (!startTerrains.Contains(currentTerrain))
+            {
+                startTerrains.Add(currentTerrain); // Adding new terrain to start terrain array making it always visible
+                RemoveFromSearch(currentTerrain);
+            }
+        }
+
+        private void RemoveFromSearch(TerrainPiece newTerrain)
+        {
+            for(int i = 0; i < allTerrains.Length; i++)
+            {
+                for (int j = 0; j < allTerrains[i].connectedTerrains.Count; j ++)
+                {
+                    for (int k = 0; k < startTerrains.Count; k++)
+                    {
+                        if (allTerrains[i].connectedTerrains[j].GetComponent<Terrain>().terrainData.Equals(startTerrains[k].GetComponent<Terrain>().terrainData))
+                        {
+                            Debug.Log("Inside first IF");
+                            if (startTerrains[k].transform.parent.Equals(newTerrain.transform.parent))
+                           {
+                                Debug.Log("Inside second IF");
+                                allTerrains[i].connectedTerrains.Remove(allTerrains[i].connectedTerrains[j]);
+                            }
+                            
+                        }
+                     }
+                 }
+            }
         }
 
         private void UpdateVisibleTerrain() {
@@ -74,14 +100,17 @@ namespace Midgaard
             float lowestDist = Vector3.Distance(currentTerrain.connectedTerrains[0].worldCenter, pm.transform.position);
             TerrainPiece lowestDistancePiece = currentTerrain.connectedTerrains[0];
 
-            for (int i = 1; i < currentTerrain.connectedTerrains.Count; i++) {
-                float dist = Vector3.Distance(currentTerrain.connectedTerrains[i].worldCenter, pm.transform.position);
-                if (dist < lowestDist) {
-                    lowestDist = dist;
-                    lowestDistancePiece = currentTerrain.connectedTerrains[i];
-                }
+            for (int i = 1; i < currentTerrain.connectedTerrains.Count; i++)
+            {
+              
+                    float dist = Vector3.Distance(currentTerrain.connectedTerrains[i].worldCenter, pm.transform.position);
+                    if (dist < lowestDist)
+                    {
+                        lowestDist = dist;
+                        lowestDistancePiece = currentTerrain.connectedTerrains[i];
+                    }
+                
             }
-
             if (!lowestDistancePiece.gameObject.activeInHierarchy) {
                for (int i = 0; i < currentTerrain.connectedTerrains.Count; i++) {
                   if(!startTerrains.Contains(currentTerrain.connectedTerrains[i]))
