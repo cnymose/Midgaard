78f1290a91a4f8ec8e7814a8b4a5f8fd2af61454
Jabbaspalace <kormind@hotmail.com>
2017-05-05 10:57:27 +0200

    Lukas stuff

--- Assets/Scripts/TerrainManager/TerrainPiece.cs
+++ Assets/Scripts/TerrainManager/TerrainPiece.cs
@@ -6,8 +6,11 @@ public class TerrainPiece : MonoBehaviour {
     public List<TerrainPiece> connectedTerrains;
     public Vector3 worldCenter;
     public GameObject coordinateCube;
+    private GameObject self;
 	
     public void CalculateWorldPosition() {
+        
+        coordinateCube = gameObject.transform.Find("CoordinateCube").gameObject;
         worldCenter = coordinateCube.transform.position;
         
     }
