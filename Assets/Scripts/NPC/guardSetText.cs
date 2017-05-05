160eac9705e1a549632f1b5fcde09e8f87e3ec3c
Prygl <lrni13@student.aau.dk>
2017-05-05 12:04:11 +0200

    små ændringer

--- Assets/Scripts/NPC/guardSetText.cs
+++ Assets/Scripts/NPC/guardSetText.cs
@@ -8,6 +8,7 @@ namespace Midgaard
     {
         public NPC_Proxy npcProx;
         private string[] strings = { "Move along...", "Find shelter, stranger...", "I would not venture out at night. Creatures lurk in the darkness!" };
+        public AudioClip[] clips;
 
 
         public void setText()
@@ -15,6 +16,7 @@ namespace Midgaard
             var rnd = Random.Range(0, strings.Length);
             npcProx.settings.conversation[0].text = strings[rnd];
             npcProx.settings.conversation[0].endsConversation = true;
+            npcProx.settings.clip = clips[rnd];
             Start_Conversation();
 
         }
