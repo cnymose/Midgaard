f186ee8bc97a549558d929e246d7ae161483b1ef
JabbasPalace <kormind@hotmail.com>
2017-05-08 01:03:56 +0200

    Forest ændringer

---
+++ Assets/Scripts/AudioScripts/KennaForestTrigger01.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KennaForestTrigger01 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("KennaVoice01").gameObject.GetComponent<AudioSource>().Play();
+        transform.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/AudioScripts/KennaForestTrigger02.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KennaForestTrigger02 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("KennaVoice02").gameObject.GetComponent<AudioSource>().Play();
+        transform.gameObject.SetActive(false);
+    }
+}
