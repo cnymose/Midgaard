5fc6ccf1dbced5093d95346af01d5d732b9c26cb
Jabbaspalace <kormind@hotmail.com>
2017-05-07 23:39:33 +0200

    HAha Sam

--- Assets/Scripts/TerrainManager/TerrainSpawnManager.cs
+++ Assets/Scripts/TerrainManager/TerrainSpawnManager.cs
@@ -2,124 +2,133 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-public class TerrainSpawnManager : MonoBehaviour
+namespace Midgaard
 {
-    public int currentArea = 0;
-    public List<TerrainPiece> spawnableArea;
-    public List<TerrainPiece> lockedPiece;
-    public List<GameObject> areas;
-    private PlayerMovement pm;
-    private GameObject player;
-    private TerrainPiece currentPiece;
-    private GameObject previousPiece;
-    private GameObject[] exits = new GameObject[3];
-    private bool calculateDistance = true;
-
-    public void Start()
+    public class TerrainSpawnManager : MonoBehaviour
     {
-        player = GameObject.Find("Player").gameObject;
-        pm = player.GetComponent<PlayerMovement>();
-        lockedPiece.Add(pm.currentTerrain);
-        currentPiece = pm.currentTerrain;
-        previousPiece = currentPiece.transform.gameObject;
+        public int currentArea = 0;
+        // public List<TerrainPiece> spawnableArea;
+       // public List<TerrainPiece> lockedPiece;
+        public List<GameObject> areas;
+        private PlayerMovement pm;
+        private GameObject player;
+        public TerrainPiece currentPiece;
+        private GameObject previousPiece;
+        private GameObject[] exits = new GameObject[3];
+        private bool calculateDistance = true;
 
-        exits[0] = currentPiece.transform.parent.FindChild("Exit " + 1).gameObject;
-        exits[1] = currentPiece.transform.parent.FindChild("Exit " + 2).gameObject;
-        exits[3] = currentPiece.transform.parent.FindChild("Exit " + 3).gameObject;
-
-        StartCoroutine(areaCheck());
-
-    }
-
-  IEnumerator areaCheck()
-    {
-        while (calculateDistance)
+        void Start()
         {
-            yield return new WaitForSeconds(5f);
-            if (!currentPiece.Equals(pm.currentTerrain))
-            {
-               
-                currentPiece = pm.currentTerrain;
-                previousPiece = currentPiece.transform.gameObject;
-                currentArea++;
-                lockedPiece.Add(currentPiece);
-
-                exits[0] = currentPiece.transform.parent.FindChild("Exit " + 1).gameObject;
-                exits[1] = currentPiece.transform.parent.FindChild("Exit " + 2).gameObject;
-                exits[3] = currentPiece.transform.parent.FindChild("Exit " + 3).gameObject;
-
-                foreach (GameObject obj in areas)
-                {
-                    if (obj.Equals(currentPiece.gameObject))
-                    {
-                        areas.Remove(obj);
-                    }
-                }
-            }
-
-            distanceToExit();
-
-        }
-    }
-    public void distanceToExit()
-    {
-        bool iterate = true;
-        float shortestDistance = 0;
-        GameObject closestExit = null;
-        int j = 0;
-
-        while (iterate)
-        {
-            if (exits[j] != null)
-            {
-                shortestDistance = Vector3.Distance(player.transform.position, exits[j].transform.position);
-                closestExit = exits[j];
-                iterate = false;
-            }
-            else
-            {
-               j++;
-            }
+            Debug.Log("Start");
+            player = GameObject.Find("Player").gameObject;
+            pm = player.GetComponent<PlayerMovement>();
             
-        }
-        for(int i = 0; i < exits.Length; i++)
-        {
-            float dist = Vector3.Distance(player.transform.position, exits[i].transform.position);
+           
+            
+            previousPiece = currentPiece.transform.gameObject;
+          
+            exits[0] = currentPiece.transform.FindChild("Exit " + 1).gameObject;
+            exits[1] = currentPiece.transform.FindChild("Exit " + 2).gameObject;
+            exits[2] = currentPiece.transform.FindChild("Exit " + 3).gameObject;
 
-            if(dist < shortestDistance)
+
+            StartCoroutine(areaCheck());
+
+        }
+
+        IEnumerator areaCheck()
+        {
+            while (calculateDistance)
             {
-                shortestDistance = dist;
-                closestExit = exits[i];
+                Debug.Log("Ja vi kører");
+                yield return new WaitForSeconds(5f);
+                if (!currentPiece.Equals(pm.currentTerrain))
+                {
+
+                    currentPiece = pm.currentTerrain;
+                    previousPiece = currentPiece.transform.gameObject;
+                    currentArea++;
+                   // lockedPiece.Add(currentPiece);
+
+                    exits[0] = currentPiece.transform.FindChild("Exit " + 1).gameObject;
+                    exits[1] = currentPiece.transform.FindChild("Exit " + 2).gameObject;
+                    exits[2] = currentPiece.transform.FindChild("Exit " + 3).gameObject;
+
+               /*     foreach (GameObject obj in areas)
+                    {
+                        if (obj.Equals(currentPiece.gameObject))
+                        {
+                            areas.Remove(obj);
+                        }
+                    } */
+                }
+
+                distanceToExit();
+
+            }
+            yield return null;
+        }
+        public void distanceToExit()
+        {
+            bool iterate = true;
+            float shortestDistance = 0;
+            GameObject closestExit = null;
+            int j = 0;
+            Debug.Log("DistanceToExit");
+            while (iterate)
+            {
+                if (exits[j] != null)
+                {
+                    shortestDistance = Vector3.Distance(player.transform.position, exits[j].transform.position);
+                    closestExit = exits[j];
+                    iterate = false;
+                }
+                else
+                {
+                    j++;
+                }
+
+            }
+            for (int i = 0; i < exits.Length; i++)
+            {
+                float dist = Vector3.Distance(player.transform.position, exits[i].transform.position);
+
+                if (dist < shortestDistance)
+                {
+                    shortestDistance = dist;
+                    closestExit = exits[i];
+                }
+
+            }
+            spawnArea(closestExit);
+
+        }
+
+
+        public void spawnArea(GameObject exit)
+        {
+            Debug.Log("SpawnArea" + exit.transform.name);
+            var area = areas[currentArea];
+            var rotation = area.transform.rotation;
+            var temp = exit;
+            area.transform.rotation = previousPiece.transform.rotation;
+
+            if (exit.transform.name == "Exit 1")
+            {
+                area.transform.RotateAround(area.transform.position, area.transform.up, 270);
             }
 
+            if (exit.transform.name == "Exit 3")
+            {
+                area.transform.RotateAround(area.transform.position, area.transform.up, 90);
+            }
+
+            var dir = exit.transform.position - area.transform.FindChild("Enter").transform.position;
+            area.transform.position += dir;
+            area.SetActive(true);
+
         }
-        spawnArea(closestExit);
-        
-    }
-
-
-    public void spawnArea(GameObject exit)
-    {
-        var area = areas[currentArea];
-        var rotation = area.transform.rotation;
-        var temp = exit;
-        area.transform.rotation = previousPiece.transform.rotation;
-
-        if (exit.transform.name == "Exit 1")
-        {
-            area.transform.RotateAround(area.transform.position, area.transform.up, 270);
-        }
-        
-        if (exit.transform.name == "Exit 3")
-        {
-            area.transform.RotateAround(area.transform.position, area.transform.up, 90);
-        }
-
-        var dir = exit.transform.position - area.transform.FindChild("Enter").transform.position;
-        area.transform.position += dir; 
 
 
     }
-
-
 }
