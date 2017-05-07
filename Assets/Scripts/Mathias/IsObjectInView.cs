ab959a4c9547795e684f964a6000753cc89ab5cf
MathiasFornitz <mathias.fornitz@gmail.com>
2017-05-08 01:13:51 +0200

    some random stuff

---
+++ Assets/Scripts/Mathias/IsObjectInView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsObjectInView : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public Camera cam;
+	public GameObject obj;
+	public bool onScreen;
+	// Update is called once per frame
+	void Update () {
+		Vector3 screenPoint = cam.WorldToViewportPoint(obj.transform.position);
+		onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+	}
+
+}
---
+++ Assets/Scripts/Mathias/PlayMovie.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMovie : MonoBehaviour {
+
+	// Use this for initialization
+	public string SceneName;
+	void Start () {
+		myMovie.Play();
+		aud.Play();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if (!myMovie.isPlaying) {
+			if (myMovie.name == "Intro") {
+				Application.LoadLevel (SceneName);
+			}
+			Debug.Log ("slut");
+		}
+	}
+
+
+	public MovieTexture myMovie;
+	public AudioSource aud;
+
+	private IEnumerator Wait(float duration)
+	{
+		yield return new WaitForSeconds(duration);
+	}
+
+	void OnGUI() {
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), myMovie);
+
+	}
+
+
+
+
+}
